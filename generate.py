e84f7f23a313203a632ce2b6a6f650658ffdc620
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-07 19:27:41 -0400

    fin hw3

--- generate.py
+++ generate.py
@@ -6,8 +6,10 @@ height = 1
 x = 0
 y = 0
 z = height/2.0
-mod = 1
-for i in range(0,9):	
-	pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length*mod,width*mod,height*mod])
-	mod = mod * 0.9
+for i in range(0,5):
+	for j in range(0,5):
+		mod = 1
+		for k in range(0,9):	
+			pyrosim.Send_Cube(name="Box", pos=[x+i,y+j,z+k] , size=[length*mod,width*mod,height*mod])
+			mod = mod * 0.9
 pyrosim.End()
