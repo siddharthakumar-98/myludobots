9fcdb0cece5052675a14f1d95fcc441b078c8e73
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-07 19:39:48 -0400

    renamed box to world

--- generate.py
+++ generate.py
@@ -1,15 +1,10 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("world.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
-z = height/2.0
-for i in range(0,5):
-	for j in range(0,5):
-		mod = 1
-		for k in range(0,9):	
-			pyrosim.Send_Cube(name="Box", pos=[x+i,y+j,z+k] , size=[length*mod,width*mod,height*mod])
-			mod = mod * 0.9
+z = height/2.0	
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -5,7 +5,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("world.sdf")
 for i in range(0,1000):
 	p.stepSimulation()
 	print(i)
