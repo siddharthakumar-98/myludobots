29a654bc79290afb562ae9ed1c1aae8e173f0a03
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-07 21:34:26 -0400

    links 0,1,2,3 done

--- generate.py
+++ generate.py
@@ -17,12 +17,13 @@ def Create_Robot():
 	length = 1
 	width = 1
 	height = 1
-	x = 0
-	y = 0
-	z = height/2.0
-	pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
-	pyrosim.Send_Cube(name="Link1", pos=[1,0,1] , size=[length,width,height])
+	pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[length,width,height])
+	pyrosim.Send_Cube(name="Link1", pos=[0,0,1] , size=[length,width,height])
+	pyrosim.Send_Cube(name="Link2", pos=[0,0,1.5] , size=[length,width,height])
+	pyrosim.Send_Cube(name="Link3", pos=[0,0.5,1.5] , size=[length,width,height])
 	pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,0.5])
+	pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,0.5])
+	pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0])
 	pyrosim.End()
 	
 Create_World()
