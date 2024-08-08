78915199f68a4f0f0bbaaab998d7ed2f260911cd
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-08 18:32:41 -0400

    updated to trach front and back leg

--- analyze.py
+++ analyze.py
@@ -1,1 +1,8 @@
-i
+import matplotlib.pyplot as plt
+import numpy as np
+
+bl_data = np.load("data/backLegSensorValues.npy")
+fl_data =  np.load("data/frontLegSensorValues.npy")
+plt.plot(bl_data)
+plt.plot(fl_data)
+plt.show()
