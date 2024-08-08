31efb50432f9950cebb8acbf6130b9e4d088726f
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-08 18:48:48 -0400

    hw sensors complete

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@ import numpy as np
 
 bl_data = np.load("data/backLegSensorValues.npy")
 fl_data =  np.load("data/frontLegSensorValues.npy")
-plt.plot(bl_data)
-plt.plot(fl_data)
+plt.plot(bl_data, label="backleg", linewidth=1)
+plt.plot(fl_data, label="frontleg")
+plt.legend()
 plt.show()
