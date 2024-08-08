d5d4a1e75216aa504f382776e9af10b9233e580c
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-07 20:05:05 -0400

    gen rob block and moved environ block

--- generate.py
+++ generate.py
@@ -1,10 +1,27 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("world.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = height/2.0	
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.End()
+
+
+def Create_World():
+	pyrosim.Start_SDF("world.sdf")
+	length = 1
+	width = 1
+	height = 1
+	x = -3
+	y = 3
+	z = height/2.0	
+	pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+	pyrosim.End()
+
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	length = 1
+	width = 1
+	height = 1
+	x = 0
+	y = 0
+	z = height/2.0
+	pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+	pyrosim.End()
+	
+Create_World()
+Create_Robot()
