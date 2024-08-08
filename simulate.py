2bf573fe1216fb5d56847bf71c1cdcf5e7a58f9d
Siddhartha Kumar <siddharthakumar@ip0af5d8e3.int.uvm.edu>
2024-08-08 19:06:32 -0400

    added motor to torso backleg

--- simulate.py
+++ simulate.py
@@ -17,6 +17,12 @@ for i in range(0,500):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_BackLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = 0.0,
+	maxForce = 500)
 	time.sleep(1/60.0)
 print(backLegSensorValues)
 np.save("data/backLegSensorValues.npy",backLegSensorValues)
