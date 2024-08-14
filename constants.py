b7fefd4ac0e5d19ba393d8a898f3172c484f15e6
Siddhartha Kumar <siddharthakumar@Siddharthas-MacBook-Air.local>
2024-08-14 10:57:27 -0400

    adding constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+import math
+
+amplitudeFrontLeg = math.pi / 4
+frequencyFrontLeg = 10
+phaseOffsetFrontLeg = 0
+
+amplitudeBackLeg = math.pi / 4
+frequencyBackLeg = 10
+phaseOffsetBackLeg = math.pi / 4   
