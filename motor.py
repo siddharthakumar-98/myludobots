50973f02bb140e537c7d278e6dcf5d0d2d4408ac
Siddhartha Kumar <siddharthakumar@Siddharthas-MacBook-Air.local>
2024-08-18 18:45:59 -0400

    step 77

--- motor.py
+++ motor.py
@@ -22,14 +22,12 @@ class MOTOR:
 		
 		self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(self.targetMin, self.targetMax, num=c.num_steps, endpoint=True) + self.offset)
 
-	def Set_Value(self, t, robotId):
+	def Set_Value(self, desiredAngle, robotId):
 		pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,
                             jointName = self.jointName,
                             controlMode = p.POSITION_CONTROL,
-                            targetPosition = self.motorValues[t],
+                            targetPosition = desiredAngle,
                             maxForce = c.maxForce)
-		if t+1 == c.num_steps:
-			self.Save_Values()
 
 	def Save_Values(self):
 		np.save('data/{self.jointName}_MotorValues.npy', self.motorValues)
--- robot.py
+++ robot.py
@@ -35,7 +35,9 @@ class ROBOT:
 	
 		for neuronName in self.nn.Get_Neuron_Names():
 			if self.nn.Is_Motor_Neuron(neuronName):
-				print(neuronName)
+				jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+				desiredAngle = self.nn.Get_Value_Of(neuronName)
+				self.motors[jointName].Set_Value(desiredAngle, self.robotId)
 	'''
 		for jointName, motor in self.motors.items():
 			motor.Set_Value(t, self.robotId)
